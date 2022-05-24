52af850d61531d46d5cf297c808818ef6d239207
GonzaloToscano90 <zalo.jat@gmail.com>
2022-05-24 03:20:09 -0300

    se agrego slider a la seccion de fer y se renombro carpeta js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//// Slider HEADER////
+//// Slider HEADER////LAUTARO
 const slidesHeaderImages = document.querySelectorAll('.fondo-header .slides-header-img');
 const nextImageDelay = 5000;
 let currentImageCounter = 0;
@@ -14,25 +14,41 @@ function nextImage() {
     currentImageCounter = (currentImageCounter + 1) % slidesHeaderImages.length;
     // slidesHeaderImages[currentImageCounter].style.display = 'block';
     slidesHeaderImages[currentImageCounter].style.opacity = 0.7;
-
-
 }
-//// Slider SHOP STORE////
+////END Slider HEADER////LAUTARO
+
+
+// SLIDER SCROLL TOP-GAME // FER // 
+const sliderTopGame = document.querySelector('.slider-top-game')
+const prevTopGame = document.querySelector('.prev-top-game')
+const nextTopgame = document.querySelector('.next-top-game')
+
+prevTopGame.addEventListener('click', () => {
+    sliderTopGame.scrollLeft -= 280
+})
+
+nextTopgame.addEventListener('click', () => {
+        sliderTopGame.scrollLeft += 280
+    })
+    //END SLIDER SCROLL TOP-GAME // FER // 
+
+
+//// Slider SHOP STORE////ZALO
 const prev = document.querySelector('.prev-shop-store')
 const next = document.querySelector('.next-shop-store')
 const slider = document.querySelector('.slider-shop-store')
 
 prev.addEventListener('click', () => {
     slider.scrollLeft -= 189
-        // alert('Slider')
 })
 
 next.addEventListener('click', () => {
-    slider.scrollLeft += 189
-})
+        slider.scrollLeft += 189
+    })
+    ////END Slider SHOP STORE////ZALO
+
 
 ////Subtitulo y texto////
-
 const platform1 = document.querySelector('.platform1')
 const platform2 = document.querySelector('.platform2')
 const platform3 = document.querySelector('.platform3')
@@ -46,6 +62,7 @@ platform2.addEventListener('click', () => {
     document.querySelector('.subtitle').innerHTML = "XBOX<br> in all GCC countries"
 })
 platform3.addEventListener('click', () => {
-    document.querySelector('.text').innerHTML = " Consectetur officiis doloribus voluptatem. Asperiores corporis exercitationem, quos nesciunt eligendi omnis cum expedita impedit ab."
-    document.querySelector('.subtitle').innerHTML = "Nintendo Wii<br> in all GCC countries"
-})
+        document.querySelector('.text').innerHTML = " Consectetur officiis doloribus voluptatem. Asperiores corporis exercitationem, quos nesciunt eligendi omnis cum expedita impedit ab."
+        document.querySelector('.subtitle').innerHTML = "Nintendo Wii<br> in all GCC countries"
+    })
+    ////END Subtitulo y texto////
